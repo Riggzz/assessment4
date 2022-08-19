7d1133f7efc9fb1a9a2850ce800543686c2fb931
= <=>
2022-08-19 10:16:26 -0600

    checkpoint1

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,15 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["A faithful friend is a strong defense.", "An important person will offer you support.", "Be careful or you could fall for some tricks today", "Believe it can be done", "Congratulations! You are on your way."];
+
+        let randomizer = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomizer];
+
+        res.status(200).send(randomFortune);
+    },
 
 }
